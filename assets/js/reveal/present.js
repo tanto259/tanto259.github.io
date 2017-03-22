7acf1c5a75aeabd31d8190cf2bdc39f14d02667b
tanto259 <contact@tanto259.name>
2017-03-22 14:46:33 +0700

    Reveal.js Implementation

---
+++ assets/js/reveal/present.js
@@ -0,0 +1,9 @@
+// Full list of configuration options available at:
+// https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+	controls: true,
+	progress: true,
+	history: true,
+	center: true,
+	transition: 'slide', // none/fade/slide/convex/concave/zoom
+});
---
+++ assets/js/reveal/print.js
@@ -0,0 +1,7 @@
+var link = document.createElement( 'link' );
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = window.location.search.match( /print-pdf/gi ) ? 'https://tanto259.name/css/reveal/pdf.css' : 'https://tanto259.name/css/reveal/paper.css' ;
+link.integrity = window.location.search.match( /print-pdf/gi ) ? 'sha512-BIUwppm1A+iFFEUCf7eGD/ZZnyQbaf8/w0UJ6UOqvHzgd3jiuKB6I18DmAIZ7+XzteI09wwgWq9dcSjB49YNhA==' : 'sha512-0rc/pmg5ec+OT9yghwTDeOeIPR0qCeDpWAsMI4SIWTUcX1EBYG/w7OfG5vin16Sb0YgvS5jSCC8g+zD+BONMGQ==';
+link.crossOrigin = 'anonymous';
+document.getElementsByTagName( 'head' )[0].appendChild( link );
