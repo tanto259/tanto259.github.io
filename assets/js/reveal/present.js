7c3b7fe4e9759033ba7c99408be97d6912c7e511
tanto259 <contact@tanto259.name>
2017-05-13 12:46:18 +0700

    Remove Reveal.js

--- assets/js/reveal/present.js
+++
@@ -1,9 +0,0 @@
-// Full list of configuration options available at:
-// https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-	controls: true,
-	progress: true,
-	history: true,
-	center: true,
-	transition: 'slide', // none/fade/slide/convex/concave/zoom
-});
